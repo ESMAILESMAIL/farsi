e743801ac3976ba060fe03199bea54d9a88a3d98
ESMAILESMAIL <sokot121@chmail.ir>
2016-04-26 14:44:22 +0430

    Update insudo.lua

--- plugins/insudo.lua
+++ plugins/insudo.lua
@@ -1,6 +1,6 @@
 do
 local function run(msg, matches)
-local sudo = 122774063 
+local sudo = 108800687 
     if matches[1] == "insudo" and is_owner(msg) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
     end
