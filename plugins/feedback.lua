e344713e03d2d2d38113b1240f2381bb3748e4a3
ESMAILESMAIL <sokot121@chmail.ir>
2016-04-26 14:34:43 +0430

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -19,7 +19,7 @@ local fuses = '!printf user#id' .. msg.from.id
  else
 
 
-                 local sends0 = send_msg('chat#36515907', fuse, ok_cb, false)
+                 local sends0 = send_msg('chat#108800687', fuse, ok_cb, false)
 
  return 'ارسال شد :)'
 
